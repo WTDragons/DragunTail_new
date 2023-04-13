c77dfd6b0c940e8bbf130c4749bec5a228512f93
111019027 <s111019027@stu.ntue.edu.tw>
2023-04-13 21:22:34 +0800

    csssss

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import 'antd/dist/reset.css'
+import React from 'react';
+import 'antd/dist/reset.css';
 import './App.css'
-import Home from '/src/pages/home'
+import {Layout, Menu, theme } from 'antd';
 
-function App() {
+const { Header, Content, Footer } = Layout;
+const App = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
   return (
-      <Home/>
-  )
-}
-
-export default App
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={new Array(15).fill(null).map((_, index) => {
+            const key = index + 1;
+            return {
+              key,
+              label: `nav ${key}`,
+            };
+          })}
+        />
+      </Header>
+      <Content
+        style={{
+          padding: '0 50px',
+        }}
+      >
+       
+        <div
+          className="site-layout-content"
+          style={{
+            background: colorBgContainer,
+          }}
+        >
+          Content
+        </div>
+      </Content>
+      <Footer
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        Ant Design ©2023 Created by Ant UED
+      </Footer>
+    </Layout>
+  );
+};
+export default App;
---
+++ src/pages/Test/Test.jsx
@@ -0,0 +1,15 @@
+import { Col, Row } from 'antd';
+import style from './Test.module.css';
+import { Grid, Tag } from 'antd'
+
+
+const Test = () => {
+  return (
+  <div className={style.container}>
+    <div className={style.Test}> 
+      testing
+    </div>
+  </div>
+  );
+};
+export default Test;
